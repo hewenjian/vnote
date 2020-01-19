64b935ad7d1b481b274b94e8e05f4c2bed2937a3
hewenjian <396735079@qq.com>
2020-01-19 20:34:59 +0800

    tag list 使能tag“多选”功能

--- src/vtagexplorer.cpp
+++ src/vtagexplorer.cpp
@@ -51,6 +51,7 @@ void VTagExplorer::setupUI()
 	m_tagList->setViewMode(QListView::IconMode);
 	m_tagList->setMovement(QListView::Static);
 	m_tagList->setSpacing(0);
+	m_tagList->setSelectionMode(QAbstractItemView::MultiSelection);
 	
     connect(m_tagList, &QListWidget::itemClicked,
             this, [this](const QListWidgetItem *p_item) {
